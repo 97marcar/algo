b9f1c8f2c6cb7c1ae00266b3524171e0022bddfe
bugmana <arostr-5@student.ltu.se>
2017-11-07 10:40:37 +0100

    Add files via upload

--- quickSort_rand.py
+++ quickSort_rand.py
@@ -1,27 +1,16 @@
-
-##def quickSortdidnotfckinwork(array):
-##
-##    if not array:
-##        return array
-##
-##    pivot = array[random.randint(0, len(array)-1)]
-##    less = [i for i in array[1:] if i < pivot]
-##    greater = [i for i in array[1:] if i >= pivot]
-##
-##    return quickSort(less) + [pivot] + quickSort(greater)
 import random, time
 
 
-def quickSort2(array): #This shit works
+def quickSort2(array): 
     less = []
     equal = []
     greater= []
 
-    if not array:
+    if not array:   # Basecase
         return array
     
-    pivot = array[random.randint(0, len(array)-1)]
-    for i in array:
+    pivot = array[random.randint(0, len(array)-1)] # Choose random pivot
+    for i in array: # Place each element in resp. stack
         if i < pivot:
             less.append(i)
         if i == pivot:
@@ -29,8 +18,9 @@ def quickSort2(array): #This shit works
         if i > pivot:
             greater.append(i)
 
-    return quickSort2(less) + equal + quickSort2(greater)
+    return quickSort2(less) + equal + quickSort2(greater) # Recursive call, sort the stacks
 
+# Mackes Testelito
 array = [2,7,3,8,1,6,4,7,3,8]
 size = 1000000
 array = [random.randrange(1, size) for _ in range(0, size)]
